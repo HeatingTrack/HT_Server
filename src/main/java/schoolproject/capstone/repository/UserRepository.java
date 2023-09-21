89e341ec832013bb87c75a59b15047b8599e4d22
KwangHo <skh990427@naver.com>
2023-09-21 22:55:50 +0900

    로그인 익셉션

--- src/main/java/schoolproject/capstone/repository/UserRepository.java
+++ src/main/java/schoolproject/capstone/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import schoolproject.capstone.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    public User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
