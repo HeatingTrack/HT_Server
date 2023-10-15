1d1cb8b3da35195c91bf2d3fa7f4c267167bbcca
KwangHo <skh990427@naver.com>
2023-10-15 23:41:57 +0900

    회원가입 RequestDto 추가

--- src/main/java/schoolproject/capstone/dto/request/UserSignupRequestDto.java
+++ src/main/java/schoolproject/capstone/dto/request/UserSignupRequestDto.java
@@ -3,7 +3,9 @@ package schoolproject.capstone.dto.request;
 import lombok.Getter;
 
 @Getter
-public class UserDto {
+public class UserSignupRequestDto {
     private String email;
     private String password;
+    private String name;
+    private String phone;
 }
