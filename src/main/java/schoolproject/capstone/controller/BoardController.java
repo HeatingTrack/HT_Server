e815bd21598dc7cf62e635d555095a9362e4d105
KwangHo <skh990427@naver.com>
2023-10-23 17:41:42 +0900

    Transactional 어노테이션 추가 - update

--- src/main/java/schoolproject/capstone/controller/BoardController.java
+++ src/main/java/schoolproject/capstone/controller/BoardController.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+import schoolproject.capstone.dto.request.BoardDeleteRequestDto;
 import schoolproject.capstone.dto.request.BoardUpdateRequestDto;
 import schoolproject.capstone.dto.request.BoardWriteRequestDto;
+import schoolproject.capstone.dto.response.BoardDeleteResponseDto;
 import schoolproject.capstone.dto.response.BoardListResponseDto;
 import schoolproject.capstone.dto.response.BoardUpdateResponseDto;
 import schoolproject.capstone.dto.response.BoardWriteResponseDto;
@@ -32,5 +34,10 @@ public class BoardController {
     @GetMapping("/list")
     public Page<BoardListResponseDto> boardList(@PageableDefault(size = 10, sort = "num", direction = Sort.Direction.DESC) Pageable pageable) {
         return boardService.boardList(pageable);
+    }
+
+    @DeleteMapping("/delete")
+    public BoardDeleteResponseDto boardDelete(@RequestBody BoardDeleteRequestDto boardDeleteRequestDto) {
+        return boardService.boardDelete(boardDeleteRequestDto);
     }
 }
